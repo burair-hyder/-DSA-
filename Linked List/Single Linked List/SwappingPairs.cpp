284ca9a2c896e9c40458e7263986a4ae834e1e2d
Syed Muhammad  Burair Hyder <burairhyder519@gmail.com>
2025-09-21 01:08:06 +0500

    Create SwappingPairs.cpp

---
+++ Linked List/Single Linked List/SwappingPairs.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+using namespace std;
+
+class Node {
+public:
+    int data;
+    Node* next;
+
+    Node(int val) {
+        data = val;
+        next = NULL;
+    }
+};
+
+Node* insertAtTail(Node* head, int val) {
+    Node* newNode = new Node(val);
+    if (head == NULL) {
+        return newNode;
+    }
+    Node* temp = head;
+    while (temp->next != NULL) {
+        temp = temp->next;
+    }
+    temp->next = newNode;
+    return head;
+}
+
+void printList(Node* head) {
+    Node* temp = head;
+    while (temp != NULL) {
+        cout << temp->data;
+        if (temp->next != NULL) cout << " -> ";
+        temp = temp->next;
+    }
+    cout << endl;
+}
