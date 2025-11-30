38ada4f46eb53d455ba56d6d03f3529ed76c1b0e
Syed Muhammad  Burair Hyder <burairhyder519@gmail.com>
2025-11-30 20:41:53 +0500

    Create bruteforce.cpp

---
+++ Strings/Pattern Matching/BruteForce Algo/bruteforce.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+
+int bruteforcealgo(string text,string pat){
+	int n  = text.length();
+	int m = pat.length();
+	
+	for (int i=0;i<n-m;i++){
+		
+		int j=0;
+		while (j<m && text[i+j]== pat[j]){
+			j++;
+		}
+		if (j==m){
+			cout <<i<<" ";
+		}
+	}
+}
+int main(){
+	string text = "the quick brown fox jumps over the lazy dog";
+	string pat = "the";
+	cout <<"Found at following Index: ";
+	bruteforcealgo(text,pat);
+	
+	
+}
+
